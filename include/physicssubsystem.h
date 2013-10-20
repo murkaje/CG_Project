3d89d06cd44346fd4d740e8bf44ca0306582c9f8
unknown <saadusman17@gmail.com>
2013-10-20 15:57:46 +0300

    physics layout

---
+++ include/physicssubsystem.h
@@ -0,0 +1,18 @@
+#ifndef PHYSICSSUBSYSTEM_H
+#define PHYSICSSUBSYSTEM_H
+
+#include<object.h>
+
+
+class PhysicsSubsystem
+{
+    public:
+        PhysicsSubsystem();
+        virtual ~PhysicsSubsystem();
+        static void PerformPhysicsChecks();
+        static void checkIntersection(Object &obj);
+    protected:
+    private:
+};
+
+#endif // PHYSICSSUBSYSTEM_H
---
+++ src/physicssubsystem.cpp
@@ -0,0 +1,67 @@
+#include "physicssubsystem.h"
+#include<scenemanager.h>
+#include<vector3f.h>
+#include<math.h>
+#include<iostream>
+using namespace std;
+
+#define v3f vector3f
+
+PhysicsSubsystem::PhysicsSubsystem()
+{
+    //ctor
+}
+
+PhysicsSubsystem::~PhysicsSubsystem()
+{
+    //dtor
+}
+
+ void PhysicsSubsystem::PerformPhysicsChecks()
+{
+    Scene* sceneObj = &SceneManager::testScene;
+    std::list<Object> &objects = sceneObj->getObjsList();
+    for (std::list<Object>::iterator iterObj = objects.begin(); iterObj != objects.end(); iterObj++)
+    {
+        checkIntersection((Object &)iterObj);
+    }
+}
+
+ void PhysicsSubsystem::checkIntersection(Object &obj)
+{
+    using namespace std;
+    Transform *objT = (Transform*)obj.getComponent(Component::TRANSFORM);
+    Scene* sceneObj = obj.getCurrentScene();
+    std::list<Object> &objects = sceneObj->getObjsList();
+    for (std::list<Object>::iterator iterObj = objects.begin(); iterObj != objects.end(); iterObj++)
+    {
+        Transform *iterObjT = (Transform*)iterObj->getComponent(Component::TRANSFORM);
+
+        if(iterObj->name != obj.name && (*iterObj).name == "cube")
+        {
+//            float euclideanDistanceBetweenObjects = sqrt(pow((objT->position.x - iterObjT->position.x),2) +
+//                                                    pow((objT->position.y - iterObjT->position.y),2) +
+//                                                    pow((objT->position.z - iterObjT->position.z),2));
+
+            float xDistance = fabs(objT->position.x - iterObjT->position.x);
+            float yDistance = fabs(objT->position.y - iterObjT->position.y);
+            float zDistance = fabs(objT->position.z - iterObjT->position.z);
+
+            float xMinDistance = (objT->scale.x + iterObjT->scale.x)/float(2);
+            float yMinDistance = (objT->scale.y + iterObjT->scale.y)/float(2);
+            float zMinDistance = (objT->scale.z + iterObjT->scale.z)/float(2);
+
+//                cout<<xDistance<<" x min: "<<xMinDistance<<endl;
+//                cout<<yDistance<<" y min: "<<yMinDistance<<endl;
+//                cout<<zDistance<<" z min: "<<zMinDistance<<endl;
+            MeshRenderer *mr = (MeshRenderer*)(*iterObj).getComponent(Component::RENDERER);
+            if(xDistance < xMinDistance && yDistance < yMinDistance && zDistance < zMinDistance)
+            {
+                mr->color = v3f(1,0,0);
+                cout<<"Colliding with "<<iterObj->name<<" !!!"<<endl;
+            } else {
+                mr->color = v3f(0,0,1);
+            }
+        }
+    }
+}
